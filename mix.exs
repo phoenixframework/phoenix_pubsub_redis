0170ab15a60bfe5db4d51a1effe03917269de1b6
Chris McCord <chris@chrismccord.com>
2015-05-28 08:24:53 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixPubsubRedis.Mixfile do
   use Mix.Project
 
-  @version "0.0.1"
+  @version "0.1.0"
 
   def project do
     [app: :phoenix_pubsub_redis,
@@ -9,7 +9,12 @@ defmodule PhoenixPubsubRedis.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     package: package,
+     deps: deps,
+     source_url: "https://github.com/phoenixframework/phoenix_pubsub_redis",
+     description: """
+     The Redis PubSub adapter for the Phoenix framework
+     """]
   end
 
   def application do
@@ -17,8 +22,15 @@ defmodule PhoenixPubsubRedis.Mixfile do
   end
 
   defp deps do
-    [{:phoenix, github: "phoenixframework/phoenix", branch: "cm-extract-redis-pubsub"},
+    # TODO bump phoenix version on next release
+    [{:phoenix, github: "phoenixframework/phoenix"},
      {:redo, "~> 2.0.1"},
      {:poolboy, "~> 1.5.1 or ~> 1.6"}]
+  end
+
+  defp package do
+    [contributors: ["Chris McCord"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/phoenixframework/phoenix_pubsub_redis"}]
   end
 end
