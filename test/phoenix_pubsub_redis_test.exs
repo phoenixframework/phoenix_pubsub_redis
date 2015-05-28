3c819e5d52cd7e18f4a720a6dacb29884edbe599
Chris McCord <chris@chrismccord.com>
2015-05-27 22:24:39 -0400

    Update for changed pubsub test location

--- test/phoenix_pubsub_redis_test.exs
+++ test/phoenix_pubsub_redis_test.exs
@@ -1,2 +1,2 @@
 Application.put_env(:phoenix, :pubsub_test_adapter, Phoenix.PubSub.Redis)
-Code.require_file "../deps/phoenix/test/support/pubsub_setup.exs", __DIR__
+Code.require_file "../deps/phoenix/test/shared/pubsub_test.exs", __DIR__
