5aac6c75bed3d8802f65c4bd3f5265d1ac25f5dd
Matt Hodgson <matthodgson@mac.com>
2016-06-23 14:55:47 -0400

    Update to work with phoenix_pubsub

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule PhoenixPubsubRedis.Mixfile do
   end
 
   defp deps do
-    [{:phoenix, "~> 1.1"},
+    [{:phoenix_pubsub, "~> 1.0.0"},
      {:redo, "~> 2.0.1"},
      {:ex_doc, "~> 0.11.1", only: :docs},
      {:poolboy, "~> 1.5.1 or ~> 1.6"}]
