2835395d518588a477978f1d342693713e9766a8
Thiago Santos <thia.mdossantos@gmail.com>
2019-11-28 13:22:38 -0300

    Allow socket opts on ssl

--- lib/phoenix_pubsub_redis/redis.ex
+++ lib/phoenix_pubsub_redis/redis.ex
@@ -32,7 +32,7 @@ defmodule Phoenix.PubSub.Redis do
 
   @behaviour Phoenix.PubSub.Adapter
   @redis_pool_size 5
-  @redis_opts [:host, :port, :password, :database, :ssl]
+  @redis_opts [:host, :port, :password, :database, :ssl, :socket_opts]
   @defaults [host: "127.0.0.1", port: 6379]
 
   ## Adapter callbacks
